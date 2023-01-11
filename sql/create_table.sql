d8dcd6fbf87dea2e37044bd71c0721def1646c8f
iida-r <iidar@insightech.co.jp>
2023-01-11 10:08:52 +0900

    modified:   build.gradle 	modified:   sql/create_table.sql 	modified:   src/main/resources/application.properties

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,11 +1,9 @@
 DROP TABLE IF EXISTS login_user;
 CREATE TABLE login_user (
-  id            BIGINT AUTO_INCREMENT           COMMENT 'ユーザーID',
+  id            BIGINT PRIMARY KEY AUTO_INCREMENT           COMMENT 'ユーザーID',
   name          VARCHAR(60) NOT NULL            COMMENT 'ユーザー名',
-  email         VARCHAR(120) NOT NULL           COMMENT 'メールアドレス',
+  email         VARCHAR(120) UNIQUE NOT NULL           COMMENT 'メールアドレス',
   password      VARCHAR(120) NOT NULL           COMMENT 'パスワード',
   roles         VARCHAR(120)                    COMMENT 'ロール',
-  enable_flag   TINYINT(1) NOT NULL DEFAULT 1   COMMENT '1:有効 0:無効',
-  PRIMARY KEY (id),
-  UNIQUE KEY (email)
+  enable_flag   TINYINT NOT NULL DEFAULT 1   COMMENT '1:有効 0:無効'
 );
